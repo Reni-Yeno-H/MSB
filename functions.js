22f7b00a02e9dca886cc50febc14a2a719a82fec
Reni Yeno H <yuanhaoren@yahoo.com>
2023-07-07 12:57:55 -0400

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,9 @@
+"use strict";
+/*
+
+    The Wings Group
+    Author: Hao Ren Yuan
+    Date: June 6, 2023
+
+    Filename: functions.js
+*/
---
+++ user_system.js
@@ -0,0 +1,9 @@
+"use strict";
+/*
+
+    The Wings Group
+    Author: Hao Ren Yuan
+    Date: June 6, 2023
+
+    Filename: user_system.js
+*/
